edd77f3250623942c4943a96731be989abbbac64
Matthew Chan <mc2438@cornell.edu>
2022-12-20 00:42:42 -0500

    Create lab test form template

--- client/src/screens/labTests.jsx
+++ client/src/screens/labTests.jsx
@@ -4,6 +4,9 @@ import {
   useTheme,
   TextField,
   LinearProgress,
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
 } from "@mui/material";
 
 function LabTests() {
@@ -16,21 +19,66 @@ function LabTests() {
       }}
     >
       <LinearProgress variant="determinate" value={5} />
-      <Box align="center" px="30px">
-        <Typography variant="h1">Select lab tests</Typography>
-        <Typography variant="subtitle1" mb="15px">
-          <Box fontWeight="bold" display="inline" component="span">
-            Hint:{" "}
-          </Box>
-          if you are unsure about which labs you need, check your prescription
-          or contact your doctor.
+      <Box px="30px">
+        <Box align="center">
+          <Typography variant="h2">Select lab tests</Typography>
+          <Typography variant="subtitle1" mb="15px">
+            <Box fontWeight="bold" display="inline" component="span">
+              Hint:{" "}
+            </Box>
+            if you are unsure about which labs you need, check your prescription
+            or contact your doctor.
+          </Typography>
+          <TextField
+            fullWidth
+            id="filled-basic"
+            label="Search lab tests"
+            variant="filled"
+          />
+          <Typography variant="h3">Select all that apply</Typography>
+        </Box>
+      </Box>
+      <Box>
+        <Typography variant="h4" px="30px">
+          Hematology
         </Typography>
-        <TextField
-          fullWidth
-          id="filled-basic"
-          label="Search lab tests"
-          variant="filled"
-        />
+        <Box px="30px" sx={{ backgroundColor: theme.palette.purple.light }}>
+          <FormGroup>
+            <FormControlLabel
+              control={<Checkbox />}
+              label={
+                <Box>
+                  <Typography>Blood Film for Malaria</Typography>
+                  <Typography variant="caption">
+                    Price range: GH$1 - GH$2
+                  </Typography>
+                </Box>
+              }
+            />
+            <FormControlLabel
+              control={<Checkbox />}
+              label={
+                <Box>
+                  <Typography>Blood Film for Malaria</Typography>
+                  <Typography variant="caption">
+                    Price range: GH$1 - GH$2
+                  </Typography>
+                </Box>
+              }
+            />
+            <FormControlLabel
+              control={<Checkbox />}
+              label={
+                <Box>
+                  <Typography>Blood Film for Malaria</Typography>
+                  <Typography variant="caption">
+                    Price range: GH$1 - GH$2
+                  </Typography>
+                </Box>
+              }
+            />
+          </FormGroup>
+        </Box>
       </Box>
     </Box>
   );
--- client/src/theme.js
+++ client/src/theme.js
@@ -9,15 +9,31 @@ const black = "#000000";
 
 const theme = createTheme({
   typography: {
+    fontSize: 18.75, // TODO: figure out mapping from rem to px for this to be exactly 16px
     fontFamily: ["Roboto"],
-    h1: {
+    h2: {
       fontWeight: 400,
       fontSize: "20px",
       margin: "20px",
     },
     subtitle1: {
+      fontWeight: 400,
       fontSize: "14px",
       lineHeight: "16px",
+    },
+    h3: {
+      fontWeight: 400,
+      fontSize: "16px",
+      margin: "20px",
+    },
+    h4: {
+      fontWeight: 500,
+      fontSize: "16px",
+      marginTop: "10px",
+      marginBottom: "10px",
+    },
+    caption: {
+      fontSize: "14px",
     },
   },
   palette: {
@@ -54,6 +70,23 @@ const theme = createTheme({
           input: {
             fontSize: "16px",
           },
+        },
+      },
+    },
+    MuiCheckbox: {
+      styleOverrides: {
+        root: {
+          "&.Mui-checked": {
+            color: darkPurple,
+          },
+        },
+      },
+    },
+    MuiFormControlLabel: {
+      styleOverrides: {
+        root: {
+          paddingTop: "10px",
+          paddingBottom: "10px",
         },
       },
     },
