c48f121271b3e20850400f148be183af89801757
Matthew Chan <mc2438@cornell.edu>
2022-12-20 23:07:34 -0500

    Add button and modularize test list

--- client/src/screens/labTests.jsx
+++ client/src/screens/labTests.jsx
@@ -8,12 +8,53 @@ import {
   Checkbox,
   InputAdornment,
   Alert,
+  Button,
 } from "@mui/material";
 import ProgressHeader from "../components/progressHeader";
 import SearchIcon from "@mui/icons-material/Search";
+import { useNavigate } from "react-router-dom";
+
+const tests = {
+  Hematology: [
+    ["Blood Film for Malaria", 1, 2],
+    ["Blood grouping", 1, 2],
+    ["Clotting profile (Pt, Aptt, D-dimers)", 1, 2],
+    ["ESR", 1, 2],
+    ["FBC", 1, 2],
+    ["Retro Screen", 1, 2],
+    ["Sickle cell electrophoresis", 1, 2],
+    ["UPT", 1, 2],
+  ],
+  Microbiology: [
+    ["H. Pylori", 1, 2],
+    ["HVS R/E", 1, 2],
+    ["Stool RE", 1, 2],
+    ["Urine RE", 1, 2],
+    ["Widal test", 1, 2],
+  ],
+  Biochemistry: [
+    ["AFP", 1, 2],
+    ["Calcium estimates", 1, 2],
+    ["CRP", 1, 2],
+    ["Electrolytes", 1, 2],
+    ["FSH", 1, 2],
+    ["HBA1c", 1, 2],
+    ["LH", 1, 2],
+    ["Lipids", 1, 2],
+    ["Liber Function test", 1, 2],
+    ["Oestrogen", 1, 2],
+    ["OGTT", 1, 2],
+    ["Progesterone", 1, 2],
+    ["PSA", 1, 2],
+    ["Renal Function test", 1, 2],
+    ["TFT (TSH, T3, T4)", 1, 2],
+    ["Uric Acid", 1, 2],
+  ],
+};
 
 function LabTests() {
   const theme = useTheme();
+  const navigate = useNavigate();
 
   return (
     <Box>
@@ -45,47 +86,44 @@ function LabTests() {
         </Box>
         <Alert severity="error">Please select at least one test</Alert>
       </Box>
-      <Box>
-        <Typography variant="h5" px="30px">
-          Hematology
-        </Typography>
-        <Box px="30px" sx={{ backgroundColor: theme.palette.purple.light }}>
-          <FormGroup>
-            <FormControlLabel
-              control={<Checkbox />}
-              label={
-                <Box>
-                  <Typography>Blood Film for Malaria</Typography>
-                  <Typography variant="caption">
-                    Price range: GH$1 - GH$2
-                  </Typography>
-                </Box>
-              }
-            />
-            <FormControlLabel
-              control={<Checkbox />}
-              label={
-                <Box>
-                  <Typography>Blood Film for Malaria</Typography>
-                  <Typography variant="caption">
-                    Price range: GH$1 - GH$2
-                  </Typography>
-                </Box>
-              }
-            />
-            <FormControlLabel
-              control={<Checkbox />}
-              label={
-                <Box>
-                  <Typography>Blood Film for Malaria</Typography>
-                  <Typography variant="caption">
-                    Price range: GH$1 - GH$2
-                  </Typography>
-                </Box>
-              }
-            />
-          </FormGroup>
-        </Box>
+      {Object.keys(tests).map((title) => {
+        return (
+          <Box mb="30px">
+            <Typography variant="h5" px="30px">
+              {title}
+            </Typography>
+            <Box px="30px" sx={{ backgroundColor: theme.palette.purple.light }}>
+              <FormGroup>
+                {tests[title].map(([test, lo, hi]) => {
+                  return (
+                    <FormControlLabel
+                      control={<Checkbox />}
+                      label={
+                        <Box>
+                          <Typography>{test}</Typography>
+                          <Typography variant="caption">
+                            Price range: GH${lo} - GH${hi}
+                          </Typography>
+                        </Box>
+                      }
+                    />
+                  );
+                })}
+              </FormGroup>
+            </Box>
+          </Box>
+        );
+      })}
+      <Box align="center">
+        <Button
+          onClick={() => navigate("/labtests")}
+          sx={{
+            ...theme.purpleButton,
+            px: "40px",
+          }}
+        >
+          Next
+        </Button>
       </Box>
     </Box>
   );
