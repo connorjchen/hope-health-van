4d5d3fabe6abb19755871520fa04779cb97f8921
Ryan Ho <apexial@aol.com>
2023-01-01 23:56:13 -0500

    async function wait fix

--- server/sendWhatsAppMessage.js
+++ server/sendWhatsAppMessage.js
@@ -1,14 +1,16 @@
 const wbm = require('wbm')
 const puppeteer = require('puppeteer')
 
-const browser = await puppeteer.launch({
-    executablePath: '/full/path/to/chrome'
-});
+
 
 // WhatsApp Message Integration
 const sendWhatsAppMessage = function(){
     wbm.start()
     .then(async () => {
+
+        const browser = await puppeteer.launch({
+            executablePath: '/full/path/to/chrome'
+        });
         const phones = ["+16095539005"];
         const message = "message here";
         await wbm.send(phones, message);
