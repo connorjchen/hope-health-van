da135f81e653e3d1dd57cfec2a94570202582884
Ryan Ho <apexial@aol.com>
2023-01-02 00:20:22 -0500

    uninstall chrome-aws-lambda and puppeteer

--- server/server.js
+++ server/server.js
@@ -5,6 +5,42 @@ const dotenv = require('dotenv')
 const routeUrl = require('./routes/routes')
 const cors = require('cors')
 
+let chrome = {};
+let puppeteer;
+
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  chrome = require("chrome-aws-lambda");
+  puppeteer = require("puppeteer-core");
+} else {
+  puppeteer = require("puppeteer");
+}
+
+app.get("/api", async (req, res) => {
+  let options = {};
+
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    options = {
+      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    };
+  }
+
+  try {
+    let browser = await puppeteer.launch(options);
+
+    let page = await browser.newPage();
+    await page.goto("https://www.google.com");
+    res.send(await page.title());
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+});
+
+
 dotenv.config()
 
 mongoose.connect(process.env.DATABASE_ACCESS, () => console.log("====== Database Connected ======"))
