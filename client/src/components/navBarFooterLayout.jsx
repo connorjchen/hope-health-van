44afa8bb0563220c3c3dd11ad14574979d393a93
Connor Chen <connorjchen2784@gmail.com>
2023-01-03 18:35:45 -0500

    Added modals for need help and lab services

--- client/src/components/navBarFooterLayout.jsx
+++ client/src/components/navBarFooterLayout.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import NavBar from "./navBar";
 import Footer from "./footer";
 import { Outlet } from "react-router-dom";
+import NeedHelpButton from "./needHelpButton";
 class NavBarFooterLayout extends React.Component {
   render() {
     return (
       <>
+        {window.location.href.includes("booking") && <NeedHelpButton />}
         <NavBar />
         <Outlet />
         <Footer />
